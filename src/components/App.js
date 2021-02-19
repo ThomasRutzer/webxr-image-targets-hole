211f856d0b4548a6afd1c157e65c51f84b7e3fc6
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-02-19 15:01:05 +0100

    Add interval to generate new balls to App

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,22 @@ import React, { useRef, useState } from "react"
 import { ARCanvas } from "@react-three/xr"
 import { trackableImage as createTrackableImage } from "./../services/xr"
 
-import trackingImageAsset from "./../trackingImages/hiro.png"
+import useStore from "./../store"
 import Scene from "./Scene"
+import useInterval from "./../utils/useInterval"
 
 function App() {
   const image = useRef()
   const [trackableImage, setTrackableImage] = useState()
+  const store = useStore()
+
+  useInterval(
+    () => {
+      // if (!imageTrackingResult) return
+      store.addBall()
+    },
+    3000
+  )
 
   const doCreateTrackableImage = async () => {
     try {
@@ -20,23 +30,34 @@ function App() {
 
   return (
     <>
-      <img
-        onLoad={() => doCreateTrackableImage()}
-        width="300"
-        height="300"
-        alt="is cool"
-        ref={image}
-        src={trackingImageAsset} />
-      { trackableImage &&
+      {/* <div className="intro">
+        <img
+          className="trackedImage"
+          onLoad={() => doCreateTrackableImage()}
+          width="300"
+          height="300"
+          alt="is cool"
+          ref={image}
+          src={process.env.PUBLIC_URL + "/trackedImage.png"} />
+        <ol className="explanation">
+          <li>Currently only available in Chrome for Android &gt;=89, with flag <span className="snippet">chrome://flags#webxr-incubations</span> active</li>
+          <li><button onClick={() => window.print()}>Print image</button> (or view on a 2<sup>nd</sup> device)</li>
+          <li>Start AR</li>
+          <li>Scan image on flat surface</li>
+        </ol>
+      </div>
+
+      { trackableImage && */}
         <ARCanvas
-          // camera={{ position: [5, 10, 0] }}
+          style={{ position: "absolute", left: 0, top: 0 }}
+          camera={{ position: [5, 10, 0] }}
           sessionInit={{
             requiredFeatures: ["image-tracking"],
-          trackedImages: [trackableImage]
+            trackedImages: [trackableImage]
           }}>
           <Scene />
         </ARCanvas>
-      }
+      {/* } */}
     </>
   )
 }
