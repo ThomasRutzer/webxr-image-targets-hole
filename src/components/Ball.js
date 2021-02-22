5c73bffd09f8bb3c3e9aaaaa061d5944f30d5429
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-02-22 22:24:27 +0100

    Update ball texture

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,4 +1,4 @@
-import { TextureLoader, RepeatWrapping, VertexColors } from "three"
+import { TextureLoader } from "three"
 import { useLoader } from "react-three-fiber"
 import { useSphere } from "@react-three/cannon"
 import { a } from '@react-spring/three'
@@ -18,12 +18,7 @@ const Ball = ({ name, startPos, opacity }) => {
         transparent
         opacity={opacity}
         attach="material"
-        // vertexColors={VertexColors}
         map={map}
-        // normalScale={[1, 1]}
-        // normalMap-wrapS={RepeatWrapping}
-        // normalMap-wrapT={RepeatWrapping}
-        // normalMap-repeat={[10, 10]}
       />
     </a.mesh>
   )
