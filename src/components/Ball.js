f55a9d2bf79fb2d40fa17eecb18b808637c559d0
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-02-22 22:24:15 +0100

    Update index.html meta data

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,27 +1,31 @@
 import { TextureLoader, RepeatWrapping, VertexColors } from "three"
 import { useLoader } from "react-three-fiber"
 import { useSphere } from "@react-three/cannon"
+import { a } from '@react-spring/three'
 
-const Ball = ({ name, startPos }) => {
+const Ball = ({ name, startPos, opacity }) => {
   const map = useLoader(TextureLoader, '/ballMaterial.jpg')
   const [ref] = useSphere(() => ({ mass: 1, position: startPos, args: 0.1 }))
 
   return (
-    <mesh
+    <a.mesh
+     receiveShadow
       castShadow 
       name={name}
       ref={ref}>
       <sphereBufferGeometry attach="geometry" args={[0.1, 64, 64]} />
-      <meshPhongMaterial
+      <a.meshBasicMaterial
+        transparent
+        opacity={opacity}
         attach="material"
-        vertexColors={VertexColors}
-        normalMap={map}
-        normalScale={[1, 1]}
-        normalMap-wrapS={RepeatWrapping}
-        normalMap-wrapT={RepeatWrapping}
-        normalMap-repeat={[10, 10]}
+        // vertexColors={VertexColors}
+        map={map}
+        // normalScale={[1, 1]}
+        // normalMap-wrapS={RepeatWrapping}
+        // normalMap-wrapT={RepeatWrapping}
+        // normalMap-repeat={[10, 10]}
       />
-    </mesh>
+    </a.mesh>
   )
 }
 
