c9ad1894e7086d7a518f408cb6c52de0d67a70eb
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-02-22 22:24:40 +0100

    Add min / max params to randomRange

--- src/utils/randomRange.js
+++ src/utils/randomRange.js
@@ -1,6 +1,6 @@
-const randomRange = () => {
-  const result = Number(Math.random().toFixed(1))
-  return result
+const randomRange = (min, max) => {
+  const result = min + (max - min) * Math.random()
+  return Number(result.toFixed(1))
 }
 
 export default randomRange
