0a060e2c82f548fce2f3bda840d2fc4a5312dfd3
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-02-26 17:14:19 +0100

    Intro layout and style update

--- src/components/App.js
+++ src/components/App.js
@@ -20,21 +20,65 @@ function App() {
   return (
     <>
       <div className="intro">
-        <h1 className="title">WebXR Image Target – Hole 🕳️</h1>
-        <img
-          className="trackedImage"
-          onLoad={() => doCreateTrackableImage()}
-          width="300"
-          height="300"
-          alt="is cool"
-          ref={image}
-          src={process.env.PUBLIC_URL + "/trackedImage.jpg"} />
-        <ol className="explanation">
-          <li><button onClick={() => window.print()}>Print image</button>  10cm X 10cm</li>
-          <li>Start AR</li>
-          <li>Scan image on flat surface</li>
-          <li>Currently only available in Chrome for Android &gt;=89, with flag <span className="snippet">chrome://flags#webxr-incubations</span> active</li>
-        </ol>
+        <section>
+          <h1 className="title">WebXR Image Target – Hole 🕳️</h1>
+          <img
+            className="trackedImage"
+            onLoad={() => doCreateTrackableImage()}
+            alt="is cool"
+            ref={image}
+            src={process.env.PUBLIC_URL + "/trackedImage.jpg"} />
+        </section>
+        <section>
+          <ol className="explanation">
+            <li>
+              <button
+                onClick={() => window.print()}>
+                Print image
+                  <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#e2e2e2"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  strokeLinejoin="arcs"
+                >
+                  <path d="M7 17l9.2-9.2M17 17V7H7" />
+                </svg>
+              </button>  10cm x 10cm
+            </li>
+            <li>Start AR</li>
+            <li>Scan image on flat surface</li>
+          </ol>
+
+        </section>
+
+        <section>
+          <hr />
+          <small className="compatibility">
+            Currently only available in Chrome for Android &gt;=89, with flag <span className="snippet">chrome://flags#webxr-incubations</span> active
+          </small>
+          <small className="credit">
+            by <a href="https://thomasrutzer@github.io" target="blank">
+              Thomas Rutzer
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeWidth="2"
+                strokeLinecap="square"
+                strokeLinejoin="arcs"
+              >
+                <path d="M7 17l9.2-9.2M17 17V7H7" />
+              </svg>
+            </a>
+          </small>
+        </section>
       </div>
 
       { trackableImage &&
