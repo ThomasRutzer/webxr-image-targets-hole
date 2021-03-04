5f678fd7f87d600e311a0a4b2b369854cefe5230
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-03-04 17:40:14 +0100

    Remove material from Ball

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,11 +1,8 @@
-import { TextureLoader } from "three"
-import { useLoader } from "react-three-fiber"
 import { useSphere } from "@react-three/cannon"
 import { a } from "@react-spring/three"
 
 const Ball = ({ name, startPos, opacity }) => {
-  const map = useLoader(TextureLoader, `${process.env.PUBLIC_URL}/ballMaterial.jpg`)
-  const [ref] = useSphere(() => ({ mass: 1, position: startPos, args: 0.1 }))
+  const [ref] = useSphere(() => ({ mass: 0.5, position: startPos, args: 0.1 }))
 
   return (
     <a.mesh
@@ -14,11 +11,11 @@ const Ball = ({ name, startPos, opacity }) => {
       name={name}
       ref={ref}>
       <sphereBufferGeometry attach="geometry" args={[0.1, 64, 64]} />
-      <a.meshBasicMaterial
+      <a.meshStandardMaterial
         transparent
         opacity={opacity}
         attach="material"
-        map={map}
+        color={"#CF94AF"}
       />
     </a.mesh>
   )
