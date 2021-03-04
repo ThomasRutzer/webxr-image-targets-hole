08607ec3813a0a9fa4ef3d50c4216e1a130b1ce4
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-03-04 17:39:15 +0100

    Rename hole.glb to bowl.glb

--- src/components/Bowl.js
+++ src/components/Bowl.js
@@ -2,7 +2,7 @@ import { useTrimesh } from "@react-three/cannon"
 import { useGLTF } from "@react-three/drei"
 
 const Bowl = (props) => {
-  const { nodes } = useGLTF(`${process.env.PUBLIC_URL}/hole.glb`)
+  const { nodes } = useGLTF(`${process.env.PUBLIC_URL}/bowl.glb`)
   const geometry = nodes.Cylinder.geometry
   const vertices = geometry.attributes.position.array
   const indices = geometry.index.array
@@ -19,7 +19,7 @@ const Bowl = (props) => {
       geometry={nodes.Cylinder.geometry}
       receiveShadow
       {...props}>
-      <meshStandardMaterial opacity={0} transparent={true} />
+      <meshStandardMaterial opacity={0.7} transparent={true} />
     </mesh>
   )
 }
